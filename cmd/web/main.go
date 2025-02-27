cee8f4aca8e362d793b5c2312223dbd6ba6dd003
markaya <markoristic15@gmail.com>
2025-02-27 11:32:55 +0100

    Chapter 3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+// NOTE: You can parse flag into pre existing var in memory
+
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	var cfg config
+	flag.StringVar(&cfg.addr, "address", ":4000", "HTTP network addr")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	addr := flag.String("addr", ":4000", "Http network addr")
+	flag.Parse()
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// NOTE: Loggers
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	// NOTE: Application
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
