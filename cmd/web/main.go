d6aff445d38bfc1569ae567468b0a2ba8fd226e4
markaya <markoristic15@gmail.com>
2025-02-27 16:41:11 +0100

    Chapter 4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/markaya/snippetbox/internal/models"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // NOTE: You can parse flag into pre existing var in memory
@@ -17,23 +22,49 @@ type config struct {
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
 
 func main() {
+
 	var cfg config
 	flag.StringVar(&cfg.addr, "address", ":4000", "HTTP network addr")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	addr := flag.String("addr", ":4000", "Http network addr")
+
+	dsn := flag.String("dsn", "./snippetbox.db?_busy_timeout=5000&_journal_mode=WAL", "Sqlite db string")
+
 	flag.Parse()
 
 	// NOTE: Loggers
 	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// NOTE: Database
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	// NOTE: Application
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -43,6 +74,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ internal/models/snippets.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES (?, ?, datetime('now', 'utc'), datetime('now', 'utc', '+' || ? || ' days'))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+
+}
+
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires 
+	FROM snippets 
+	WHERE expires > datetime('now', 'utc') 
+	AND id = ?
+	`
+
+	s := &Snippet{}
+
+	row := m.DB.QueryRow(stmt, id)
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
+}
+
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	stmt := `SELECT id,title,content,created,expires FROM snippets
+    WHERE expires > datetime('now', 'utc') ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// WARNING: This is important as if when it is not closed the database connection
+	// will remain open whcih can lead to all connection pool to be used up
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
+}
